4c52bef4ae143b4b239388a21c73db0cb19bdbde
caiyukui <wenqian.cai@tuya.com>
2024-08-06 13:41:02 +0800

    ss

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/caiyukui/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/caiyukui/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/caiyukui/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/caiyukui/my-website/src/css/custom.css"),
+];
---
+++ build/assets/js/f8551aab.db8bc8a9.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2224],{1976:(e,s,a)=>{a.r(s),a.d(s,{default:()=>t});a(6540);var c=a(781),i=a(4848);function t(){return(0,i.jsxs)(c.A,{children:[(0,i.jsx)("h1",{children:"My React page"}),(0,i.jsx)("p",{children:"This is a React page"})]})}}}]);
